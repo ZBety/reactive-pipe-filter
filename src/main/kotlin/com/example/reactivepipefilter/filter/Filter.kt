72cbc067ef285cff404ee23fe58089f135c410f0
zbety <zhijie.zhou@moying.ai>
2022-10-26 18:34:04 +0800

    使用PipedInputStream初步实现

--- src/main/kotlin/com/example/reactivepipefilter/filter/Filter.kt
+++ src/main/kotlin/com/example/reactivepipefilter/filter/Filter.kt
@@ -1,4 +1,132 @@
 package com.example.reactivepipefilter.filter
 
-class Filter {
+import java.io.IOException
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
+import kotlin.system.exitProcess
+
+
+abstract class Filter : Thread() {
+    var c = 0.0 //返回参数
+    var p = 0.1
+    var t = 1.2
+    var pipedInputStream = PipedInputStream()
+    var pipedOutputStream = PipedOutputStream()
+    var returnInputData = PipedInputStream()
+    var returnOutputData = PipedOutputStream()
+    override fun run() {
+        while (true) {
+            try {
+                if (pipedInputStream.available() != 0) {
+                    val buf = ByteArray(1024)
+                    val len = pipedInputStream.read(buf)
+                    val s = String(buf, 0, len)
+                    doTask(s.toDouble())
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            yield()
+        }
+    }
+
+    abstract fun doTask(num: Double)
+}
+
+class Filter1(p: Double) : Filter() {
+    override fun doTask(num: Double) {
+        var num1 = num
+        try {
+            try {
+                if (returnInputData.available() != 0) {
+                    val buf = ByteArray(1024)
+                    val len = returnInputData.read(buf)
+                    val s = String(buf, 0, len)
+                    c = s.toDouble()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            num1 -= c * this.p!!
+            pipedOutputStream.write((num1.toString() + "").toByteArray())
+            yield()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}
+
+class Filter2(t: Double) : Filter() {
+    override fun doTask(num: Double) {
+        var num1 = num
+        try {
+            num1 *= this.t
+            pipedOutputStream.write((num1.toString() + "").toByteArray())
+            returnOutputData.write((num1.toString() + "").toByteArray()) //回去管道
+            pipedOutputStream.flush()
+            returnOutputData.flush()
+            c = num1
+            yield()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}
+
+internal class Sink : Thread() {
+    var inputStream = PipedInputStream()
+
+    override fun run() {
+        var index = 0
+        while (true) {
+            index++
+            //            System.out.println("Sink运行");
+            try {
+                /**
+                 * 1: 2.28
+                 * 2: 2.13
+                 * 3: 2.26
+                 * 4: 2.13
+                 * 5: 2.02
+                 */
+                val buf = ByteArray(1024)
+                val len = inputStream.read(buf)
+                val s = String(buf, 0, len)
+                System.out.printf("%d: %.2f", index, s.toDouble())
+                println()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            yield()
+        }
+    }
+}
+
+internal class Source : Thread() {
+    //输入数据
+    var pipedOutputStream = PipedOutputStream()
+    var list: List<Double>? = null
+
+    override fun run() {
+        for (item in list!!) {
+            try {
+                pipedOutputStream.write((item.toString() + "").toByteArray())
+                pipedOutputStream.flush()
+                try {
+                    sleep(3)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            yield()
+        }
+        try {
+            sleep(200)
+            exitProcess(0)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
 }
--- src/test/kotlin/com/example/reactivepipefilter/ReactivePipeFilterApplicationTests.kt
+++ src/test/kotlin/com/example/reactivepipefilter/ReactivePipeFilterApplicationTests.kt
@@ -1,13 +1,46 @@
 package com.example.reactivepipefilter
 
+import com.example.reactivepipefilter.filter.*
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+
 
 @SpringBootTest
 class ReactivePipeFilterApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		/*
+        * 输入代码,请保留
+        * 数组inputs对应控制台输入
+        *  a-输入数据；
+        *  p-过滤器1中的乘数因子；
+        *  t-过滤器2中的乘数因子；
+        *  */
+		val nums = listOf(1.9,2.0,2.1,2.0,1.9).toMono()
+		val inputs = listOf(1.9,2.0,2.1,2.0,1.9)
+		val p = 0.1
+		val t = 1.2
+
+		//创建数据发生器（com.example.reactivepipefilter.filter.Source）、过滤器（com.example.reactivepipefilter.filter.Filter）、数据接收器（com.example.reactivepipefilter.filter.Sink），并完成连接
+		val gen = Source()
+		gen.list = inputs
+		val filter1: Filter = Filter1(p)
+		val filter2: Filter = Filter2(t)
+		val sink = Sink()
+
+		gen.pipedOutputStream.connect(filter1.pipedInputStream)
+		filter1.pipedOutputStream.connect(filter2.pipedInputStream)
+		filter2.pipedOutputStream.connect(sink.inputStream)
+		filter2.returnOutputData.connect(filter1.returnInputData)
+		gen.start()
+		filter1.start()
+		filter2.start()
+		sink.start()
+
+		Thread.sleep(2000)
 	}
 
 }
